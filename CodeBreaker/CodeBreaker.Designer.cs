459cb10793bef0da3cbe48649a3c3994212af04b
Tor Alver Moen <tor.alver.moen@gmail.com>
2022-01-16 13:04:25 +0100

    Created the main window design
    #jira CB-11

---
+++ CodeBreaker/CodeBreaker.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace CodeBreaker
+{
+    partial class CodeBreaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CodeBreaker));
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelChooseColour = new System.Windows.Forms.Label();
+            this.btnColour0 = new System.Windows.Forms.Button();
+            this.btnColour1 = new System.Windows.Forms.Button();
+            this.btnColour2 = new System.Windows.Forms.Button();
+            this.btnColour3 = new System.Windows.Forms.Button();
+            this.btnColour4 = new System.Windows.Forms.Button();
+            this.displayColour1 = new System.Windows.Forms.Label();
+            this.displayColour2 = new System.Windows.Forms.Label();
+            this.displayColour3 = new System.Windows.Forms.Label();
+            this.labelCorrectPlacement = new System.Windows.Forms.Label();
+            this.displayCorrectPlacement = new System.Windows.Forms.Label();
+            this.labelCorrectColour = new System.Windows.Forms.Label();
+            this.displayCorrectColour = new System.Windows.Forms.Label();
+            this.labelTurns = new System.Windows.Forms.Label();
+            this.displayTurns = new System.Windows.Forms.Label();
+            this.btnDifficulty = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(784, 30);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Code Breaker";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelChooseColour
+            // 
+            this.labelChooseColour.AutoSize = true;
+            this.labelChooseColour.Location = new System.Drawing.Point(74, 73);
+            this.labelChooseColour.Name = "labelChooseColour";
+            this.labelChooseColour.Size = new System.Drawing.Size(103, 17);
+            this.labelChooseColour.TabIndex = 1;
+            this.labelChooseColour.Text = "Choose colour:";
+            // 
+            // btnColour0
+            // 
+            this.btnColour0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnColour0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColour0.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnColour0.Location = new System.Drawing.Point(74, 113);
+            this.btnColour0.Name = "btnColour0";
+            this.btnColour0.Size = new System.Drawing.Size(100, 40);
+            this.btnColour0.TabIndex = 2;
+            this.btnColour0.UseVisualStyleBackColor = false;
+            // 
+            // btnColour1
+            // 
+            this.btnColour1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnColour1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColour1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnColour1.Location = new System.Drawing.Point(74, 159);
+            this.btnColour1.Name = "btnColour1";
+            this.btnColour1.Size = new System.Drawing.Size(100, 40);
+            this.btnColour1.TabIndex = 3;
+            this.btnColour1.UseVisualStyleBackColor = false;
+            // 
+            // btnColour2
+            // 
+            this.btnColour2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnColour2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColour2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnColour2.Location = new System.Drawing.Point(74, 205);
+            this.btnColour2.Name = "btnColour2";
+            this.btnColour2.Size = new System.Drawing.Size(100, 40);
+            this.btnColour2.TabIndex = 4;
+            this.btnColour2.UseVisualStyleBackColor = false;
+            // 
+            // btnColour3
+            // 
+            this.btnColour3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnColour3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColour3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnColour3.Location = new System.Drawing.Point(74, 251);
+            this.btnColour3.Name = "btnColour3";
+            this.btnColour3.Size = new System.Drawing.Size(100, 40);
+            this.btnColour3.TabIndex = 5;
+            this.btnColour3.UseVisualStyleBackColor = false;
+            // 
+            // btnColour4
+            // 
+            this.btnColour4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnColour4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColour4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnColour4.Location = new System.Drawing.Point(74, 297);
+            this.btnColour4.Name = "btnColour4";
+            this.btnColour4.Size = new System.Drawing.Size(100, 40);
+            this.btnColour4.TabIndex = 6;
+            this.btnColour4.UseVisualStyleBackColor = false;
+            // 
+            // displayColour1
+            // 
+            this.displayColour1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.displayColour1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.displayColour1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.displayColour1.Location = new System.Drawing.Point(234, 73);
+            this.displayColour1.Name = "displayColour1";
+            this.displayColour1.Size = new System.Drawing.Size(150, 150);
+            this.displayColour1.TabIndex = 7;
+            this.displayColour1.Text = "1";
+            this.displayColour1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // displayColour2
+            // 
+            this.displayColour2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.displayColour2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.displayColour2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.displayColour2.Location = new System.Drawing.Point(390, 73);
+            this.displayColour2.Name = "displayColour2";
+            this.displayColour2.Size = new System.Drawing.Size(150, 150);
+            this.displayColour2.TabIndex = 8;
+            this.displayColour2.Text = "2";
+            this.displayColour2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // displayColour3
+            // 
+            this.displayColour3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.displayColour3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.displayColour3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.displayColour3.Location = new System.Drawing.Point(546, 73);
+            this.displayColour3.Name = "displayColour3";
+            this.displayColour3.Size = new System.Drawing.Size(150, 150);
+            this.displayColour3.TabIndex = 9;
+            this.displayColour3.Text = "3";
+            this.displayColour3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCorrectPlacement
+            // 
+            this.labelCorrectPlacement.AutoSize = true;
+            this.labelCorrectPlacement.Location = new System.Drawing.Point(234, 243);
+            this.labelCorrectPlacement.Name = "labelCorrectPlacement";
+            this.labelCorrectPlacement.Size = new System.Drawing.Size(272, 17);
+            this.labelCorrectPlacement.TabIndex = 10;
+            this.labelCorrectPlacement.Text = "Colours in the code and the correct place:";
+            // 
+            // displayCorrectPlacement
+            // 
+            this.displayCorrectPlacement.AutoSize = true;
+            this.displayCorrectPlacement.Location = new System.Drawing.Point(514, 243);
+            this.displayCorrectPlacement.Name = "displayCorrectPlacement";
+            this.displayCorrectPlacement.Size = new System.Drawing.Size(50, 17);
+            this.displayCorrectPlacement.TabIndex = 11;
+            this.displayCorrectPlacement.Text = "[place]";
+            // 
+            // labelCorrectColour
+            // 
+            this.labelCorrectColour.AutoSize = true;
+            this.labelCorrectColour.Location = new System.Drawing.Point(234, 263);
+            this.labelCorrectColour.Name = "labelCorrectColour";
+            this.labelCorrectColour.Size = new System.Drawing.Size(277, 17);
+            this.labelCorrectColour.TabIndex = 12;
+            this.labelCorrectColour.Text = "Colours in the code but in the wrong place:";
+            // 
+            // displayCorrectColour
+            // 
+            this.displayCorrectColour.AutoSize = true;
+            this.displayCorrectColour.Location = new System.Drawing.Point(514, 263);
+            this.displayCorrectColour.Name = "displayCorrectColour";
+            this.displayCorrectColour.Size = new System.Drawing.Size(55, 17);
+            this.displayCorrectColour.TabIndex = 13;
+            this.displayCorrectColour.Text = "[colour]";
+            // 
+            // labelTurns
+            // 
+            this.labelTurns.AutoSize = true;
+            this.labelTurns.Location = new System.Drawing.Point(234, 293);
+            this.labelTurns.Name = "labelTurns";
+            this.labelTurns.Size = new System.Drawing.Size(178, 17);
+            this.labelTurns.TabIndex = 14;
+            this.labelTurns.Text = "Remaining number of tries:";
+            // 
+            // displayTurns
+            // 
+            this.displayTurns.AutoSize = true;
+            this.displayTurns.Location = new System.Drawing.Point(419, 293);
+            this.displayTurns.Name = "displayTurns";
+            this.displayTurns.Size = new System.Drawing.Size(43, 17);
+            this.displayTurns.TabIndex = 15;
+            this.displayTurns.Text = "[tries]";
+            // 
+            // btnDifficulty
+            // 
+            this.btnDifficulty.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnDifficulty.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDifficulty.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDifficulty.Location = new System.Drawing.Point(80, 400);
+            this.btnDifficulty.Name = "btnDifficulty";
+            this.btnDifficulty.Size = new System.Drawing.Size(150, 40);
+            this.btnDifficulty.TabIndex = 16;
+            this.btnDifficulty.Text = "button1";
+            this.btnDifficulty.UseVisualStyleBackColor = false;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAbout.Location = new System.Drawing.Point(240, 400);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(150, 40);
+            this.btnAbout.TabIndex = 17;
+            this.btnAbout.Text = "button2";
+            this.btnAbout.UseVisualStyleBackColor = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnNewGame.Location = new System.Drawing.Point(400, 400);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(150, 40);
+            this.btnNewGame.TabIndex = 18;
+            this.btnNewGame.Text = "button3";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.Location = new System.Drawing.Point(560, 400);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 40);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "button4";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // CodeBreaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnDifficulty);
+            this.Controls.Add(this.displayTurns);
+            this.Controls.Add(this.labelTurns);
+            this.Controls.Add(this.displayCorrectColour);
+            this.Controls.Add(this.labelCorrectColour);
+            this.Controls.Add(this.displayCorrectPlacement);
+            this.Controls.Add(this.labelCorrectPlacement);
+            this.Controls.Add(this.displayColour3);
+            this.Controls.Add(this.displayColour2);
+            this.Controls.Add(this.displayColour1);
+            this.Controls.Add(this.btnColour4);
+            this.Controls.Add(this.btnColour3);
+            this.Controls.Add(this.btnColour2);
+            this.Controls.Add(this.btnColour1);
+            this.Controls.Add(this.btnColour0);
+            this.Controls.Add(this.labelChooseColour);
+            this.Controls.Add(this.labelTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "CodeBreaker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code Breaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelChooseColour;
+        private System.Windows.Forms.Button btnColour0;
+        private System.Windows.Forms.Button btnColour1;
+        private System.Windows.Forms.Button btnColour2;
+        private System.Windows.Forms.Button btnColour3;
+        private System.Windows.Forms.Button btnColour4;
+        private System.Windows.Forms.Label displayColour1;
+        private System.Windows.Forms.Label displayColour2;
+        private System.Windows.Forms.Label displayColour3;
+        private System.Windows.Forms.Label labelCorrectPlacement;
+        private System.Windows.Forms.Label displayCorrectPlacement;
+        private System.Windows.Forms.Label labelCorrectColour;
+        private System.Windows.Forms.Label displayCorrectColour;
+        private System.Windows.Forms.Label labelTurns;
+        private System.Windows.Forms.Label displayTurns;
+        private System.Windows.Forms.Button btnDifficulty;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
--- CodeBreaker/CodeBreaker.cs
+++ CodeBreaker/CodeBreaker.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CodeBreaker
 {
-    public partial class Form1 : Form
+    public partial class CodeBreaker : Form
     {
-        public Form1()
+        public CodeBreaker()
         {
             InitializeComponent();
         }
